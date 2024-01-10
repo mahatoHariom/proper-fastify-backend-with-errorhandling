d3cb8820a3f8334765a647531e0015463a97082a
Marcos Botene <marcosjbotene@gmail.com>
2024-01-10 16:25:22 -0300

    refactor: using custom env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { app } from './app';
+import { env } from './env';
 
 app
-  .listen({ host: '0.0.0.0', port: 3333 })
-  .then(() => console.log('Server running'));
+  .listen({ host: '0.0.0.0', port: env.PORT })
+  .then(() => console.log('HTTP Server running!'));
