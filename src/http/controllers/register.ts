c821b8f2402047d6e5df72a39d829ec83f7b60cb
Marcos Botene <marcosjbotene@gmail.com>
2024-01-17 15:06:29 -0300

    feat: controller to create a user

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,25 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+import { RegisterUseCase } from '@/use-cases/register';
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { email, name, password } = registerBodySchema.parse(request.body);
+
+  try {
+    const usersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    await registerUseCase.handle({ email, name, password });
+  } catch (error) {
+    return reply.status(409).send();
+  }
+
+  return reply.status(201).send();
+}
