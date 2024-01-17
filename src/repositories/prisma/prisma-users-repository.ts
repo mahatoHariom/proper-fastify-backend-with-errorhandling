0832e79e6903ca9f46844cec5c1536dbc4593029
Marcos Botene <marcosjbotene@gmail.com>
2024-01-17 15:02:12 -0300

    feat: created prisma users repository

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,17 @@
+import { prisma } from '@/libs';
+import { Prisma } from '@prisma/client';
+import { UsersRepository } from '../users-repository';
+
+export class PrismaUsersRepository implements UsersRepository {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    return user;
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({ data });
+
+    return user;
+  }
+}
