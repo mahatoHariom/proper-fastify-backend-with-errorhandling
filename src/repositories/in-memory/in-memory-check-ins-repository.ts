8753c419c7e49677bce9a69604d43cc7820c8be9
Marcos Botene <marcosjbotene@gmail.com>
2024-01-20 18:55:48 -0300

    feat: method to find a user's check in

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,5 +1,6 @@
 import { CheckIn, Prisma } from '@prisma/client';
 import { randomUUID } from 'crypto';
+import dayjs from 'dayjs';
 
 import { CheckInsRepository } from '..';
 
@@ -18,5 +19,23 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     this.items.push(checkIn);
 
     return checkIn;
+  }
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf('date');
+    const endOfTheDay = dayjs(date).endOf('date');
+
+    const checkInOnSameDate = this.items.find(checkIn => {
+      const checkInDate = dayjs(checkIn.created_at);
+
+      const isOnSameDay =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
+
+      return checkIn.user_id === userId && isOnSameDay;
+    });
+
+    if (!checkInOnSameDate) return null;
+
+    return checkInOnSameDate;
   }
 }
