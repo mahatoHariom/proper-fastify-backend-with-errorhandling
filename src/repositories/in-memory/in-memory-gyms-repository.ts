d29e2aa6e4e2aa5abd4ccc9b4d6e371102982fe7
Marcos Botene <marcosjbotene@gmail.com>
2024-01-21 20:59:38 -0300

    feat: method to create a new gym in the in-memory gym repository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,9 +1,25 @@
-import { Gym } from '@prisma/client';
+import { Gym, Prisma } from '@prisma/client';
+import { randomUUID } from 'crypto';
 
 import { GymsRepository } from '../gyms-repository';
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = [];
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym: Gym = {
+      description: data.description ?? null,
+      id: data.id ?? randomUUID(),
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+      phone: data.phone ?? null,
+      title: data.title,
+    };
+
+    this.items.push(gym);
+
+    return gym;
+  }
 
   async findById(id: string) {
     const gym = this.items.find(item => item.id === id);
