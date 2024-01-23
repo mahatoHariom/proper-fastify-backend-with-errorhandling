ec12f8b1761fcb399de2c8a3deeb956975cbc370
Marcos Botene <marcosjbotene@gmail.com>
2024-01-23 16:12:44 -0300

    feat: method to find a gym by name

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -28,4 +28,10 @@ export class InMemoryGymsRepository implements GymsRepository {
 
     return gym;
   }
+
+  async searchMany(query: string, page: number) {
+    return this.items
+      .filter(item => item.title.includes(query))
+      .slice((page - 1) * 20, page * 20);
+  }
 }
