4077eb2d7cced69152b653ec15564fb1fd1421ac
Marcos Botene <marcosjbotene@gmail.com>
2024-01-23 15:29:18 -0300

    feat: method to find many check-ins by user id

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -38,4 +38,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return checkInOnSameDate;
   }
+
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter(item => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20);
+  }
 }
