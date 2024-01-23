fa2737c4693bee80688ab074a636db98ef4655e2
Marcos Botene <marcosjbotene@gmail.com>
2024-01-23 18:55:47 -0300

    feat: methods to save and search a check-in by id

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -21,6 +21,14 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkIn;
   }
 
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex(item => item.id === checkIn.id);
+
+    if (checkInIndex >= 0) this.items[checkInIndex] = checkIn;
+
+    return checkIn;
+  }
+
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf('date');
     const endOfTheDay = dayjs(date).endOf('date');
@@ -43,6 +51,14 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return this.items
       .filter(item => item.user_id === userId)
       .slice((page - 1) * 20, page * 20);
+  }
+
+  async findById(id: string) {
+    const checkIn = this.items.find(item => item.id === id);
+
+    if (!checkIn) return null;
+
+    return checkIn;
   }
 
   async countByUserId(userId: string) {
