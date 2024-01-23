4ed0ff46a7021a17dece55609071fb86d619f6a2
Marcos Botene <marcosjbotene@gmail.com>
2024-01-23 15:48:40 -0300

    test: get user metrics use case

---
+++ src/use-cases/get-user-metrics.spec.ts
@@ -0,0 +1,31 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import { InMemoryCheckInsRepository } from '@/repositories';
+
+import { GetUserMetricsUseCase } from './get-user-metrics';
+
+let checkInsRepository: InMemoryCheckInsRepository;
+let sut: GetUserMetricsUseCase;
+
+describe('Get User Metrics Use Case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository();
+    sut = new GetUserMetricsUseCase(checkInsRepository);
+  });
+
+  it('should be able to get check-ins count from metrics', async () => {
+    await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    });
+
+    await checkInsRepository.create({
+      gym_id: 'gym-02',
+      user_id: 'user-01',
+    });
+
+    const { checksInsCount } = await sut.handle({ userId: 'user-01' });
+
+    expect(checksInsCount).toEqual(2);
+  });
+});
