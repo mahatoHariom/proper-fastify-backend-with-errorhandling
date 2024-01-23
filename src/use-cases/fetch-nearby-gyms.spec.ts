e19e2a846dfac7e790822f380bea2db79fc3ee51
Marcos Botene <marcosjbotene@gmail.com>
2024-01-23 16:41:37 -0300

    test: fetch nearby gyms use case

---
+++ src/use-cases/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,41 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+
+import { InMemoryGymsRepository } from '@/repositories';
+
+import { FetchNearbyGymsUseCase } from './fetch-nearby-gyms';
+
+let gymsRepository: InMemoryGymsRepository;
+let sut: FetchNearbyGymsUseCase;
+
+describe('Fetch Nearby Gyms Use Case', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new FetchNearbyGymsUseCase(gymsRepository);
+  });
+
+  it('should be able to fetch nearby gyms', async () => {
+    await gymsRepository.create({
+      title: 'Near Gym',
+      description: null,
+      phone: null,
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    });
+
+    await gymsRepository.create({
+      title: 'Far Gym',
+      description: null,
+      phone: null,
+      latitude: -27.0610928,
+      longitude: -49.5229501,
+    });
+
+    const { gyms } = await sut.handle({
+      userLatitude: -27.2092052,
+      userLongitude: -49.6401091,
+    });
+
+    expect(gyms).toHaveLength(1);
+    expect(gyms).toEqual([expect.objectContaining({ title: 'Near Gym' })]);
+  });
+});
