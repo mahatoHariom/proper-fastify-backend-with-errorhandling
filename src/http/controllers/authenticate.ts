d7cb5b32c12c3acebbd9fbd7da0bd3a49c66a416
Marcos Botene <marcosjbotene@gmail.com>
2024-01-24 18:02:46 -0300

    feat: returning a jwt token when logging in

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -17,7 +17,11 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase();
 
-    await authenticateUseCase.handle({ email, password });
+    const { user } = await authenticateUseCase.handle({ email, password });
+
+    const token = await reply.jwtSign({}, { sign: { sub: user.id } });
+
+    return reply.status(200).send({ token });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message });
@@ -25,6 +29,4 @@ export async function authenticate(
 
     throw error;
   }
-
-  return reply.status(200).send();
 }
