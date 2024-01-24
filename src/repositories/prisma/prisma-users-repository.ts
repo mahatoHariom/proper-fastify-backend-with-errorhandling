c64794a4c4f3104be6418547e18238fcc5931ddf
Marcos Botene <marcosjbotene@gmail.com>
2024-01-24 14:55:12 -0300

    feat: method to find a user by id in the prisma users repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -5,14 +5,20 @@ import { prisma } from '@/libs';
 import { UsersRepository } from '..';
 
 export class PrismaUsersRepository implements UsersRepository {
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({ data });
+
+    return user;
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({ where: { email } });
 
     return user;
   }
 
-  async create(data: Prisma.UserCreateInput) {
-    const user = await prisma.user.create({ data });
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({ where: { id } });
 
     return user;
   }
