60dd1b7df964582ac07908e2a8f5eb6a50188a4c
Marcos Botene <marcosjbotene@gmail.com>
2024-01-24 15:41:44 -0300

    feat: created prisma gyms repository

--- src/repositories/prisma/index.ts
+++ src/repositories/prisma/index.ts
@@ -1,2 +1,3 @@
 export { PrismaCheckInsRepository } from './prisma-check-ins-repository';
+export { PrismaGymsRepository } from './prisma-gyms-repository';
 export { PrismaUsersRepository } from './prisma-users-repository';
