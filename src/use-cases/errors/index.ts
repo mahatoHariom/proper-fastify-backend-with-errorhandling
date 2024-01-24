2fbd030dd719e20ca02164465dc6b267141d561a
Marcos Botene <marcosjbotene@gmail.com>
2024-01-23 23:21:08 -0300

    feat: late check-in validation error

--- src/use-cases/errors/index.ts
+++ src/use-cases/errors/index.ts
@@ -1,4 +1,5 @@
 export { InvalidCredentialsError } from './invalid-credentials-error';
+export { LateCheckInValidationError } from './late-check-in-validation-error';
 export { MaxDistanceReachedError } from './max-distance-reached-error';
 export { MaxNumberOfCheckInsReachedError } from './max-number-of-check-ins-reached-error';
 export { ResourceNotFoundError } from './resource-not-found-error';
