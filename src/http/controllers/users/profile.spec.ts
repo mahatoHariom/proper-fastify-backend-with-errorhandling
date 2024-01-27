f84c51f1c204ec721720c8e764a3e969b09664c3
Marcos Botene <marcosjbotene@gmail.com>
2024-01-27 17:29:23 -0300

    refactor: using util in profile e2e test

--- src/http/controllers/users/profile.spec.ts
+++ src/http/controllers/users/profile.spec.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
 import { app } from '@/app';
+import { createAndAuthenticateUser } from '@/utils';
 
 describe('Profile (E2E)', () => {
   beforeAll(async () => {
@@ -13,18 +14,7 @@ describe('Profile (E2E)', () => {
   });
 
   it('should be able to get user profile', async () => {
-    await request(app.server).post('/users').send({
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password: '123456',
-    });
-
-    const authResponse = await request(app.server).post('/sessions').send({
-      email: 'johndoe@example.com',
-      password: '123456',
-    });
-
-    const { token } = authResponse.body;
+    const { token } = await createAndAuthenticateUser(app);
 
     const profileResponse = await request(app.server)
       .get('/me')
