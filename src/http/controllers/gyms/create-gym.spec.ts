43c840b5d3ca4be99666a207249484884f36c969
Marcos Botene <marcosjbotene@gmail.com>
2024-01-27 17:30:16 -0300

    test: create gym controller

---
+++ src/http/controllers/gyms/create-gym.spec.ts
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+
+import { app } from '@/app';
+import { createAndAuthenticateUser } from '@/utils';
+
+describe('Create Gym (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description.',
+        phone: '1199999999',
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
