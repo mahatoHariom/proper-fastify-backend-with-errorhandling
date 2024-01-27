8d2429b9a8e43b63eaf2a85b0c849244e3a7b1ef
Marcos Botene <marcosjbotene@gmail.com>
2024-01-27 16:16:53 -0300

    feat: new gym routes

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,6 +2,15 @@ import { FastifyInstance } from 'fastify';
 
 import { verifyJwt } from '@/http/middlewares';
 
+import { createGym } from './create-gym';
+import { fetchNearbyGyms } from './fetch-nearby-gyms';
+import { searchGyms } from './search-gyms';
+
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt);
+
+  app.get('/gyms/search', searchGyms);
+  app.get('/gyms/nearby', fetchNearbyGyms);
+
+  app.post('/gyms', createGym);
 }
