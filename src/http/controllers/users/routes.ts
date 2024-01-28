5d7df194f3991b300fa34bafbcd77f2468f74967
Marcos Botene <marcosjbotene@gmail.com>
2024-01-27 23:02:55 -0300

    test: refresh token controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,11 +4,14 @@ import { verifyJwt } from '@/http/middlewares';
 
 import { authenticate } from './authenticate';
 import { profile } from './profile';
+import { refresh } from './refresh';
 import { register } from './register';
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authenticate);
+
+  app.patch('/token/refresh', refresh);
 
   app.get('/me', { onRequest: [verifyJwt] }, profile);
 }
