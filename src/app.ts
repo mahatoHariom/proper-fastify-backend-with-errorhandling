36226bdf89e4038478cdb0a58b4a2fbc48171681
Marcos Botene <marcosjbotene@gmail.com>
2024-01-27 23:02:39 -0300

    feat: refresh token controller

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import fastifyCookie from '@fastify/cookie';
 import fastifyJwt from '@fastify/jwt';
 import { fastify } from 'fastify';
 import { ZodError } from 'zod';
@@ -7,7 +8,16 @@ import { checkInsRoutes, gymsRoutes, usersRoutes } from './http';
 
 export const app = fastify();
 
-app.register(fastifyJwt, { secret: env.JWT_SECRET });
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: { expiresIn: '10m' },
+});
+
+app.register(fastifyCookie);
 
 app.register(checkInsRoutes);
 app.register(gymsRoutes);
